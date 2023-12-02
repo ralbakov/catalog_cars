21f883d495ebf4cdd583d174a0219eef37254ab8
Ruslan Albakov <albakov.ruslan@gmail.com>
2023-12-02 15:18:48 +0300

    local change

--- myproject/idea/management/commands/update_autoru_catalog.py
+++ myproject/idea/management/commands/update_autoru_catalog.py
@@ -20,11 +20,11 @@ class Command(BaseCommand):
         soup = bs(r.text, features='xml')
         for mark in soup.find_all('mark'):
             car_mark = CarMark(name=mark['name'])
-            car_mark.save()
+
             temp_set = set()
             for model in mark.find_all('folder'):
                 temp_set.add(model['name'].split(',')[0])
             for i in temp_set:
                 car_model = CarModel(name=i, carmark=car_mark)
-                car_model.save()
+
             temp_set.clear()
